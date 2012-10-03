87d5dda9bbd520cb8636ce14bdffab5a1efa88c3
Koichi Tanaka <kou11031@gmail.com>
2012-10-03 23:09:54 +0900

    rails new . -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Clips::Application.config.secret_token = 'cf9cd6c5f2b599959416de6b89e46ed855eea0295cf44524cdf8f41a7f065eb1a1002b8b0b50439ca64fdb8961e5bdb682d6edd06e04af8047b9770d0717790f'
