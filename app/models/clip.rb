b03bd03b6a65eed40edfff8a99bd17f1114c81dd
Koichi Tanaka <k.tanaka@everyleaf.com>
2012-12-01 11:57:10 +0900

    Replacee access_id from acts_as_random_id for Clip

--- app/models/clip.rb
+++ app/models/clip.rb
@@ -1,22 +1,9 @@
 require 'securerandom'
 
 class Clip < ActiveRecord::Base
-  attr_accessible :code, :access_id
+  attr_accessible :code
 
   validates :code, presence: true
 
-  before_create :generate_access_id
-
-  ACCESS_ID_LENGTH = 10
-
-  private
-  def generate_access_id
-    # SecureRandom#hex returns string that length is twice of given number length
-    random_token = SecureRandom.hex(ACCESS_ID_LENGTH/2)
-    if self.class.where(access_id: random_token).empty?
-      self.access_id = random_token
-    else
-      generate_access_id
-    end
-  end
+  acts_as_random_id
 end
