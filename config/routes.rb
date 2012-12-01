50dd140682b6e0a8ef58e8d1c581b2e9db5bb900
Koichi Tanaka <k.tanaka@everyleaf.com>
2012-12-01 16:16:59 +0900

    Add user login with github

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Clips::Application.routes.draw do
   root to: 'clips#new'
 
   resources :clips
+
+  match 'auth/:provider/callback', to: 'sessions#create'
+  match 'auth/failure', to: redirect('/')
+  match 'signout', to: 'sessions#destory', as: 'signout'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121201025114) do
+ActiveRecord::Schema.define(:version => 20121201065316) do
 
   create_table "clips", :force => true do |t|
     t.text     "code"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
