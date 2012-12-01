510ec909f8b25cccee0ca6af0db8280a8b81c1d2
Koichi Tanaka <k.tanaka@everyleaf.com>
2012-12-01 18:01:58 +0900

    Add migration add coulmn user_id to clips

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121201065316) do
+ActiveRecord::Schema.define(:version => 20121201090030) do
 
   create_table "clips", :force => true do |t|
     t.text     "code"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|
