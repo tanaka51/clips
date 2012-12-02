00df4e1fa2b013602842a10ae7d9ddbf2c318021
Koichi Tanaka <k.tanaka@everyleaf.com>
2012-12-02 15:33:47 +0900

    Add spec for User

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,60 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".from_omniauth" do
+    subject { described_class.from_omniauth(auth) }
+
+    let!(:provider) { 'github' }
+    let!(:uid)      { '12345678' }
+    let!(:name)     { 'testuser' }
+
+    let(:auth) do
+      {
+        'provider' => 'github',
+        'uid' => uid,
+        'info' => {
+          'nickname' => name
+        }
+      }
+    end
+    let(:user) { FactoryGirl.create :user, provider: provider, uid: uid, name: name}
+
+    before do
+      described_class
+        .stub(:where)
+        .with('provider' => provider, 'uid' => uid)
+        .and_return([user])
+    end
+
+    it 'finds user from auth' do
+      described_class.from_omniauth(auth)
+      expect(described_class).to have_received(:where).with('provider' => provider, 'uid' => uid)
+    end
+
+    context "when user existes" do
+      it 'returns a user' do
+        expect(subject).to eq user
+      end
+    end
+
+    context "when user dosen't exist" do
+      before do
+        described_class.stub(:where).and_return([])
+      end
+
+      it 'creates a user' do
+        described_class.should_receive(:create!)
+        described_class.from_omniauth(auth)
+      end
+
+      it 'returns user sames with auth' do
+        user = subject
+
+        expect(user.provider).to eq provider
+        expect(user.uid)     .to eq uid
+        expect(user.name)    .to eq name
+      end
+    end
+  end
+
 end
