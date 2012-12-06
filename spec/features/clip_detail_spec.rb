851c5479cbc7291090d5e71cf8e8a717948d96ec
Koichi Tanaka <k.tanaka@everyleaf.com>
2012-12-06 20:10:08 +0900

    Display user name in Clips#show

--- spec/features/clip_detail_spec.rb
+++ spec/features/clip_detail_spec.rb
@@ -8,10 +8,13 @@ test3
 test4
   CODE
   }
-  given!(:clip) { FactoryGirl.create :clip, code: code }
+  given!(:clip) { FactoryGirl.create :clip, code: code, user: user }
+
+  given(:user) { FactoryGirl.create :user, name: name }
+  given(:name) { 'test-san' }
 
   background do
-    do_signin
+    do_signin(user)
   end
 
   scenario 'User sees highliting code' do
@@ -19,6 +22,12 @@ test4
 
     expect(page).to have_css('.CodeRay')
     expect(page).to have_text(code)
+  end
+
+  scenario 'User sees author' do
+    visit clip_path(clip)
+
+    expect(page).to have_text(name)
   end
 
   scenario 'User links to edit to click edit link' do
