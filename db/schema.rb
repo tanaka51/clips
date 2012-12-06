b31643c4f23c49ab2d11d32456b682ee5556b342
Koichi Tanaka <k.tanaka@everyleaf.com>
2012-12-06 19:15:50 +0900

    Add model Group

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121206100938) do
+ActiveRecord::Schema.define(:version => 20121206101345) do
 
   create_table "clips", :force => true do |t|
     t.text     "code"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.integer  "user_id"
+  end
+
+  create_table "groups", :force => true do |t|
+    t.string   "name",       :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "groups_users", :force => true do |t|
