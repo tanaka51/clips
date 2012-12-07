c553b1ba5c92e2d735cfda6b5159b6c18561a134
Koichi Tanaka <k.tanaka@everyleaf.com>
2012-12-07 22:25:58 +0900

    Setup webmock for rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -11,6 +11,7 @@ Spork.prefork do
   require 'rspec/rails'
   require 'rspec/autorun'
   require 'rspec-spies'
+  require 'webmock/rspec'
 
   # Requires supporting ruby files with custom matchers and macros, etc,
   # in spec/support/ and its subdirectories.
